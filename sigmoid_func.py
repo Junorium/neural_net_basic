f05f5ea89d4a0af1f569666a3e0ac61400f16544
John Richard Perez <46211205+Junorium@users.noreply.github.com>
2025-01-24 10:43:27 -0500

    Update sigmoid_func.py
    Add basic sigmoid

--- sigmoid_func.py
+++ sigmoid_func.py
@@ -1,5 +1,27 @@
 import numpy as np
 import pandas as pd
 
-def sigmoid(input, weights, bias):
-  return None
+weight_A = np.array(
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+)
+
+input_A = np.array(
+  [11,12,13]
+)
+
+bias_A = np.array(
+  [14,15,16]
+)
+
+def sigmoid(x, W, b):
+  '''
+  weights W: n x n matrix
+  input x: n size vector
+  bias b: n size vector
+  '''
+  z = np.dot(W, x) + b
+  return 1 / (1 + np.exp(-z))
+
+sigmoid(input_A, weight_A, bias_A)
